08600e823597b5c82e1cde7fb25a5cde0e593084
Andrew Sharkey <sharkand@hotmail.com>
2021-09-08 18:28:11 +0100

    Start button now in middle of map and transition working

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,32 +7,41 @@ import '../App.css';
 const Main = () => {
 
     const [pageLoaded, setPageLoaded] = useState(false);
-    const [transitionInProgress, setTransitionInProgress] = useState(true);
+    const [startPressed, setStartPressed] = useState(false);
+    const [transitionFinished, setTransitionFinished] = useState(false);
   
     useEffect(() => {
-      setPageLoaded(true);
+        setPageLoaded(true); 
         }, []);
-  
+
     useEffect(() => {
-        setTimeout( () => setTransitionInProgress(false), 4900);
-        }, [pageLoaded]);
+        if (pageLoaded) {
+        setTimeout( () => setTransitionFinished(true), 4900) };
+        }, [startPressed]);
   
     const onClick = (e) => {
       console.log(e.target.id);
     }
   
-
     return (
         <>
-            <div id="menu">
+            {/* <div id="menu">
                 <h1>
                 Menu goes here
                 </h1>
-            </div>
+            </div> */}
             <div className = 'container'>
-                {transitionInProgress && 
-                <img src={Satellite} width="1024" className='fade-out' />}
-                <Map onClick={onClick}/>
+                {(!transitionFinished) && 
+                <img src={Satellite} 
+                    width="1024" 
+                    className={startPressed? 'fade-out': 'static'} 
+                    />}
+                {(!transitionFinished) && <button type='button' 
+                        onClick = {() => setStartPressed(true)}
+                        value = "START"
+                        className={startPressed? 'fade-out': 'static'} >START</button>}
+                {startPressed &&
+                <Map onClick={onClick}/>}
              </div>
             
         </>
