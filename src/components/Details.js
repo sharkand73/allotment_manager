3850bc85ab7629ba2cc921b88ffce4c234fbd469
Andrew Sharkey <sharkand@hotmail.com>
2021-09-08 22:33:04 +0100

    Info now displaying on card.

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 const Details = ({areaSelected, setAreaSelected}) => {
     
+    let tableInfo = [];
+    for(let key in areaSelected){
+        tableInfo.push(
+            <tr>
+                <td>{key}</td>
+                <td>{areaSelected[key]}</td>
+            </tr>
+            )
+    }
 
-    
     return (
         <div className = 'details'>
             <h1 className = 'heading'>{areaSelected.name}</h1>
-            
+            <table>
+                {tableInfo}
+            </table>         
         </div>
     )
 }
