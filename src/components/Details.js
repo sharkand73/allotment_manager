c46d2d5ee0935a80cf7da07066dc4dc5f0961568
Andrew Sharkey <sharkand@hotmail.com>
2021-09-09 12:21:51 +0100

    Formatting of Details card, X button added.

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 const Details = ({areaSelected, setAreaSelected}) => {
-    
+
     let tableInfo = [];
     for(let key in areaSelected){
-        if (key !== 'id'){
+        if (key !== 'id' && areaSelected[key]){
         tableInfo.push(
             <tr key={key}>
-                <td className='key'>{key}</td>
-                <td>{areaSelected[key]}</td>
+                <td className='key'>{key}:</td>
+                
+                <td>{areaSelected[key].toString()}</td>
+                
             </tr>
             )
         }
@@ -17,6 +19,7 @@ const Details = ({areaSelected, setAreaSelected}) => {
     return (
         <div className = 'details'>
             <h1 className = 'heading'>{areaSelected.name}</h1>
+            <span className='X' onClick = {() => setAreaSelected(null)}>X</span>
             <table id='plotInfo'>
                 <tbody>
                     {tableInfo}
